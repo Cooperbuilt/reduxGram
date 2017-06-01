91ea7e9aabdb70ae757444ed0f4a51df1ee311ac
Evan Cooper <evanncooper@gmail.com>
2017-06-01 19:34:44 -0400

    Adding component dir, basic components with structure, and setting up Routes.

---
+++ client/components/Photogrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Photogrid extends React.Component {
+  
+
+  render() {
+    return (
+      <div className="photo-grid">Photo Grid</div>
+    );
+  }
+}
