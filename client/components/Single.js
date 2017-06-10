a28f144a119ef3ee7545c222bb62a048669f9f5c
Evan Cooper <evanncooper@gmail.com>
2017-06-10 10:10:26 -0400

    Construct the Comment component and build out the Single view

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Photo from './Photo';
+import Comment from './Comments'
 
 export default class Single extends React.Component {
 
   render() {
+  	// need index of the post
+  	const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+  	// get the post
+  	const post = this.props.posts[i];
+  	console.log(post);
+
     return (
       <div className = "single-photo">
-      	Single Grid
-
+      	<Photo i={i} post={post} {...this.props} />
+      	<Comment />
       </div>
     );
   }
